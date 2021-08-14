b2d9b9ec21fcb9ea1832513e74a5383c3b7ff2f7
Francisco Javier <48967040+FranJF@users.noreply.github.com>
2021-08-15 00:47:56 +0200

    Update telescope.lua

--- lua/franjf/telescope.lua
+++ lua/franjf/telescope.lua
@@ -127,4 +127,26 @@ M.git_branches = function()
     })
 end
 
+local function refactor(prompt_bufnr)
+    local content = require("telescope.actions.state").get_selected_entry(
+        prompt_bufnr
+    )
+    require("telescope.actions").close(prompt_bufnr)
+    require("refactoring").refactor(content.value)
+end
+
+M.refactors = function()
+    require("telescope.pickers").new({}, {
+        prompt_title = "refactors",
+        finder = require("telescope.finders").new_table({
+            results = require("refactoring").get_refactors(),
+        }),
+        sorter = require("telescope.config").values.generic_sorter({}),
+        attach_mappings = function(_, map)
+            map("i", "<CR>", refactor)
+            map("n", "<CR>", refactor)
+            return true
+        end
+})
+
 return M
