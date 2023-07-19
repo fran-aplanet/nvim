604ba063c3abaebc288a85740935896f83264674
FranJF <franjcfernandez@gmail.com>
2023-07-19 16:50:40 +0200

    fix

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,8 @@
+require('rose-pine').setup({
+    variant = 'moon',
+    disable_italics = true,
+})
+
  function ColorMyPencils(color) 
  	color = color or "rose-pine"
  	vim.cmd.colorscheme(color)
