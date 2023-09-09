6474e52992964fca95d87ce927642fe5cc3a5d17
FranJF]] <franjcfernandez@gmail.com>
2023-09-09 21:04:18 +0200

    improved: null

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,9 @@
-require('rose-pine').setup({
-    disable_italics = true,
-    disable_background = true,
+require("rose-pine").setup({
+	disable_italics = true,
 })
-
- function ColorMyPencils(color) 
- 	color = color or "rose-pine"
- 	vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
- end
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+end
 
 ColorMyPencils()
