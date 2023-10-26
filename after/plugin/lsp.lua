3efe50c86b6ee8c46b4709c6ef26a8cc994680b6
FranJF]] <franjcfernandez@gmail.com>
2023-10-26 13:57:34 +0200

    fix: lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,8 @@
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
 	vim.keymap.set("n", "gd", function()
 		vim.lsp.buf.definition()
 	end, opts)
@@ -13,7 +15,9 @@ lsp_zero.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>n", function()
 		vim.diagnostic.goto_next()
 	end, opts)
-	-- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("i", "<leader>h", function() 
+        vim.lsp.buf.signature_help() 
+    end, opts)
 	vim.keymap.set("n", "<leader>w", function()
 		vim.lsp.buf.workspace_symbol()
 	end, opts)
@@ -27,6 +31,12 @@ end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = { "tsserver", "pylsp" },
-	handlers = { lsp_zero.default_setup },
+	ensure_installed = { "pyright", },
+    handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
 })
